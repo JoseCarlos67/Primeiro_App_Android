2231f127e5799c15145b10970bd68a03447b0ab8
JoseCarlos67 <carlos6767jose@gmail.com>
2023-05-08 08:15:44 -0300

    First Commit

---
+++ app/src/main/java/com/example/primerioapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.primerioapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
